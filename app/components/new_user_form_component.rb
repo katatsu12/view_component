20fe646d4200f9b4ea2e0f8a684b1c0ed3f7c17f
den <katatsu12@gmail.com>
2023-03-26 21:00:56 +0300

    Add view component, new user form component

---
+++ app/components/new_user_form_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class NewUserFormComponent < ViewComponent::Base
+
+end
---
+++ test/components/new_user_form_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class NewUserFormComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(NewUserFormComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end

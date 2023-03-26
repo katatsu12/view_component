d3ab6f6e955524deff27d6632c6475c623d9df3b
den <katatsu12@gmail.com>
2023-03-26 20:59:07 +0300

    Add tailwind, new users & site controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'User successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
